9149081c58163673588e8bc71539456b66ec6fea
Pierre Kokou <pierre kokou@esa-ad.esa.int>
2017-02-14 17:06:30 +0100

    Test PK new machine -  I just added a print(PK)

--- example.py
+++ example.py
@@ -10,6 +10,8 @@ Created on Thu Feb 09 18:24:46 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
+print("PK test")
+
 x = np.linspace(0, 10, 100)
 y = np.sin(x)
 
@@ -18,4 +20,5 @@ plt.show()
 
 x = np.random.random((10,10))
 plt.matshow(x)
-plt.show()
+plt.show() 
+
