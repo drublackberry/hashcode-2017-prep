8b73dabf8667aa25ab410dcff908c18dfeb4b13c
Andreu Mora <andreu.mora@esa.int>
2017-02-21 21:41:52 +0100

    I am tired man

--- 2017_Pizza/solver.py
+++ 2017_Pizza/solver.py
@@ -9,14 +9,23 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import iolib as iolib
 
+def get_ordered_index (a, b):
+    if a <= b:
+        return range(a,b+1)
+    else:
+        return range(b,a+1)
+        
+
 class PizzaBoard:
     
     def __init__ (self):
         self.moves = {}
         self.moves['ur'] = pd.read_excel('moves.xlsx', sheetname='up_right')     
         self.moves['ul'] = pd.read_excel('moves.xlsx', sheetname='up_left')     
-        self.moves['ll'] = pd.read_excel('moves.xlsx', sheetname='low_left')     
-        self.moves['lr'] = pd.read_excel('moves.xlsx', sheetname='low_right')     
+        self.moves['ll'] = pd.read_excel('moves.xlsx', sheetname='down_left')     
+        self.moves['lr'] = pd.read_excel('moves.xlsx', sheetname='down_right')  
+        self.slice_plan = pd.DataFrame(columns=['r_min', 'r_max', 'c_min', 'c_max'])
+        self.vertices = pd.DataFrame(columns=['x', 'y', 'ul', 'ur', 'll', 'lr'])
     
     def load_from_scenario (self, scenario_in):
         dict_in = iolib.get_pizza_df(scenario_in)
@@ -27,11 +36,7 @@ class PizzaBoard:
     def create_random_pizza (self, R, C):
         self.pizza = pd.DataFrame(np.random.randint(0, high=2, size=(R,C)))  
         self.L = np.random.randint(1,5)
-        self.H = np.random.randint(5,10)
-        
-    def init_slice_plan (self, N):
-        self.slice_plan = pd.DataFrame(index=range(N), columns=['r_min', 'r_max', 'c_min', 'c_max'])
-        pass
+        self.H = np.random.randint(5,10)        
             
     def display_pizza (self):
         plt.matshow(self.pizza)
@@ -44,28 +49,39 @@ class PizzaBoard:
         print ("Number of mushrooms = {}".format(num_mushroom))
         print ("Number of tomatoes = {}".format(num_tomato))
         
-    def __check_slice_valid (self, slice_in):
+    def check_slice_valid (self, slice_in):
         num_tomato = slice_in.sum().sum()
         num_mushroom = slice_in.count().sum() - num_tomato
-        if num_tomato + num_mushroom <= self.H and num_tomato >= self.H and num_mushroom >= self.H:
+        if num_tomato + num_mushroom <= self.H and num_tomato >= self.L and num_mushroom >= self.L:
             return True
         else:
             return False
+    
+    def find_slice (self, i,j):
+        for dir_eval in self.moves.keys():                 
+            i1 = (i + self.moves[dir_eval]['delta_i'])
+            j1 = (j + self.moves[dir_eval]['delta_j'])
+            for i_end, j_end in zip(i1, j1):
+                slice_eval = self.pizza.loc[get_ordered_index(i,i_end), get_ordered_index(j,j_end)]                            
+                if self.check_slice_valid(slice_eval):                    
+                    return slice_eval
+        return None
         
-    def get_slice (self, i, j, origin='ul'):
-        if origin == 'ul':
-            step_d = np.sqrt(self.L)
-            # start growing from here
-            while i+step_d <= self.pizza.shape[0] and j+step_d <= self.pizza.shape[1]:
-                slice_out = self.pizza.loc[i:i+step_d, j]
-                if self.__check_slice_valid (slice_out):
-                    return slice_out
-                else:
-                    step_d = step_d + 1 # increment naively
-              
-board = PizzaBoard()
-board.load_from_scenario('big')
-board.get_pizza_report()
-#board.display_pizza()
-x = board.get_slice(4,4)
+    def update_vertices (self, slice_in):
+        df = pd.DataFrame(index=range(4), columns=['x', 'y', 'ul', 'ur', 'll', 'lr'])
+        for c in range(4):
+            df.loc[c,'x'] = slice_in
+        self.vertices.append()
+        
+        
+    
+
+def main():  
+    board = PizzaBoard()
+    board.load_from_scenario('big')
+    board.get_pizza_report()
+    slice_start = board.find_slice(board.pizza.shape[0]/2, board.pizza.shape[1]/2)
+    # Index the for corner os the slice in the list
+    
+
         
